6218d6636688f8b504c2f0b6826d3a4fb87909e4
Zakaria Ahmada <127176536+zakaria1909@users.noreply.github.com>
2023-03-28 10:39:24 +0700

    Add files via upload

---
+++ datadiri,variabel,komentar/komentar.js
@@ -0,0 +1,11 @@
+//variabel var
+var a ="zaka";
+console.log(a);
+let b ="19 september 2003";
+console.log(b);
+const c ="jepara";
+console.log(c);
+//tampilannya
+document.write("Nama          : "+a+"</br>");
+document.write("Tanggal Lahir : "+b+"</br>");
+document.write("Alamat        : "+c+"</br>");
